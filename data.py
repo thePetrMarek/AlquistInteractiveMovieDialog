e1c3c51a29ae1dbebc4b82adec05fb2774f30e28
Petr Marek <ermrk.marek@gmail.com>
2018-01-31 15:06:21 +0100

    Loading of structured json

--- data.py
+++ data.py
@@ -9,30 +9,68 @@ class Data(metaclass=Singleton):
     def __init__(self):
         self.answers = []
         self.fast_text_model = None
+        self._allCombinationsOfSinglePattern = []
 
     def process(self, data_file):
-        self.load_answers(data_file)
-        self.create_labeled_questions_file(data_file, "fasttext_data.txt")
+        json_object = self.load_json(data_file)
+        json_object = self.replace_entities(json_object)
+        self.load_answers(json_object)
+        self.create_labeled_questions_file(json_object, "fasttext_data.txt")
         self.fast_text_model = fastText.train_supervised("fasttext_data.txt")
 
-    def create_labeled_questions_file(self, in_file_name, out_file_name):
-        with open(in_file_name, "r", encoding="utf-8") as in_file:
+    def load_json(self, data_file):
+        with open(data_file, "r", encoding="utf-8") as in_file:
             json_object = json.load(in_file)
-            pairs = json_object["pairs"]
+        return json_object
 
-            with open(out_file_name, "w", encoding="utf-8") as out_file:
-                for i, pair in enumerate(pairs):
-                    questions = pair["question"]
-                    for question in questions:
-                        out_file.write("__label__" + str(i) + " " + question + "\n")
+    def replace_entities(self, json_object):
+        for entity in json_object["entities"]:
+            entity_name = entity["name"]
+            entity_value = entity["value"]
+            for i, pair in enumerate(json_object["pairs"]):
+                for j, question in enumerate(pair["question"]):
+                    json_object["pairs"][i]["question"][j] = question.replace("$$" + entity_name + "$$", entity_value)
+                for j, answer in enumerate(pair["answer"]):
+                    json_object["pairs"][i]["answer"][j] = answer.replace("$$" + entity_name + "$$", entity_value)
+        return json_object
 
-    def load_answers(self, in_file_name):
-        with open(in_file_name, "r", encoding="utf-8") as in_file:
-            json_object = json.load(in_file)
-            pairs = json_object["pairs"]
-            for pair in pairs:
-                answers = pair["answer"]
-                self.answers.append(answers)
+    def create_labeled_questions_file(self, json_object, out_file_name):
+        pairs = json_object["pairs"]
+        with open(out_file_name, "w", encoding="utf-8") as out_file:
+            for i, pair in enumerate(pairs):
+                questions = pair["question"]
+                for question in questions:
+                    question_all_variants = self.createAllTextVariants(question)
+                    for question_variant in question_all_variants:
+                        out_file.write("__label__" + str(i) + " " + question_variant + "\n")
+
+    def load_answers(self, json_object):
+        pairs = json_object["pairs"]
+        for pair in pairs:
+            answers = pair["answer"]
+            answers_all_variants = []
+            for answer in answers:
+                answers_all_variants += self.createAllTextVariants(answer)
+            self.answers.append(answers_all_variants)
+
+    def createAllTextVariants(self, pattern):
+        self._allCombinationsOfSinglePattern = []
+        pattern = pattern.replace(")", "(")
+        patterns = pattern.split("(")
+        patternsSplited = []
+        for pattern in patterns:
+            patternsSplited.append(pattern.split("|"))
+        self.createAllCombinations(patternsSplited, "")
+        result = self._allCombinationsOfSinglePattern
+        return result
+
+    def createAllCombinations(self, groups, seqence):
+        if len(groups) == 0:
+            self._allCombinationsOfSinglePattern.append(seqence)
+        else:
+            head, *tail = groups
+            for part in head:
+                self.createAllCombinations(tail, seqence + part)
 
     def get_answer(self, message):
         label = self.fast_text_model.predict(message)[0][0]
